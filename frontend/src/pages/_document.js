62ad7f8bba6716143f998dc6e96868e5f879c333
Irving Amaechi <leoivin0@gmail.com>
2024-07-06 04:39:50 +0100

    Modified document.js

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,7 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-
 
 export default function Document() {
   return (
