ab4a54cc833c0a6a30542bd518921bdb078584eb
Irving Amaechi <leoivin0@gmail.com>
2024-07-06 04:35:28 +0100

    Requirements.txt added

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,4 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 export default function Document() {
   return (
