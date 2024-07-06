b9dc12ee0f0e5bda1c29969831be7f72c30ca07e
Irving Amaechi <leoivin0@gmail.com>
2024-07-06 04:10:55 +0100

    CORS Update

--- backend/main.py
+++ backend/main.py
@@ -6,13 +6,7 @@ from routers import admin, users
 
 app = FastAPI()
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-     "http://localhost:3000"
-]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
